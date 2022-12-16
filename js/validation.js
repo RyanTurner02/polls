50887226e15a4534a9f63e943930fa059cc4f73c
Ryan Turner <RyanTurner02@protonmail.com>
2022-12-16 18:12:49 -0500

    Create sign up form

---
+++ js/validation.js
@@ -0,0 +1,5 @@
+let name = document.getElementById("name");
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let submitButton = document.getElementById("submit");
