5e7e7aa81aa6b46606c29acde33a9eff5d2293c9
Ryan Turner <RyanTurner02@protonmail.com>
2022-12-16 20:04:54 -0500

    Hide validation requirements until data is entered

--- js/validation.js
+++ js/validation.js
@@ -11,16 +11,23 @@ let emailField = document.getElementById("email");
 let passwordField = document.getElementById("password");
 let submitButton = document.getElementById("submit");
 
+let nameValidation = document.getElementById("name-validation");
+let usernameValidation = document.getElementById("username-validation");
+let emailValidation = document.getElementById("email-validation");
+let passwordValidation = document.getElementById("password-validation");
+
 nameField.addEventListener("keyup", validateName);
 usernameField.addEventListener("keyup", validateUsername);
 emailField.addEventListener("keyup", validateEmail);
 passwordField.addEventListener("keyup", validatePassword);
 
+hideValidationRequirements();
 toggleSubmitButton();
 
 function validateName() {
+    nameValidation.style.display = "block";
+
     if (nameField.value.length > 50) {
-        console.log("NAME IS GREATER THAN 50 CHARACTERS");
         validName = false;
     } else {
         validName = true;
@@ -29,42 +36,57 @@ function validateName() {
 }
 
 function validateUsername() {
+    usernameValidation.style.display = "block";
+
     // check if username is taken
     toggleSubmitButton();
 }
 
 function validateEmail() {
+    emailValidation.style.display = "block";
+
     // check if email is invalid
     // check if email is taken
     toggleSubmitButton();
 }
 
 function validatePassword() {
+    passwordValidation.style.display = "block";
+
+    let flag = true;
     let password = passwordField.value;
     let numberRegex = /\d/;
     let lowerCaseRegex = /[a-z]/;
     let upperCaseRegex = /[A-Z]/;
 
     if (password.length < 8) {
-        console.log("PASSWORD LESS THAN 8 CHARACTERS");
-        validPassword = false;
+        flag = false;
     }
 
     if (!numberRegex.test(password)) {
-        console.log("NUMBER NOT FOUND");
-        validPassword = false;
+        flag = false;
     }
 
     if (!lowerCaseRegex.test(password)) {
-        console.log("LOWERCASE CHARACTER NOT FOUND");
-        validPassword = false;
+        flag = false;
     }
 
     if (!upperCaseRegex.test(password)) {
-        console.log("UPPERCASE CHARACTER NOT FOUND");
-        validPassword = false;
+        flag = false;
     }
+
+    if (flag) {
+        validPassword = true;
+    }
+
     toggleSubmitButton();
+}
+
+function hideValidationRequirements() {
+    nameValidation.style.display = "none";
+    usernameValidation.style.display = "none";
+    emailValidation.style.display = "none";
+    passwordValidation.style.display = "none";
 }
 
 function hasValidInformation() {
