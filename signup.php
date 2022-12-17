31b33f5ef007f2246014a5ad5a08ceda6b2261a9
Ryan Turner <RyanTurner02@protonmail.com>
2022-12-16 20:52:58 -0500

    Display user's info after signing up

--- signup.php
+++ signup.php
@@ -1,1 +1,11 @@
 <?php
+
+$name = $_POST["name"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+echo "Name: $name" . "<br>";
+echo "Username: $username" . "<br>";
+echo "Email: $email" . "<br>";
+echo "Password: $password" . "<br>";
