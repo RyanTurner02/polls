dcead0a5c6f119fde9d18cd020e080da6d544a90
Ryan Turner <RyanTurner02@protonmail.com>
2022-12-17 18:39:23 -0500

    Create login form

---
+++ js/login-validation.js
@@ -0,0 +1,4 @@
+// element variables
+let usernameField = document.getElementById("username");
+let passwordField = document.getElementById("password");
+let loginButton = document.getElementById("login");
