b97b848a3cb3a83806d4403a8374c8f9edc04351
Ryan Turner <RyanTurner02@protonmail.com>
2022-12-25 14:49:37 -0500

    Display different navbar options if the user is logged in or not

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,18 +12,27 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="header navbar">
-        <div class="d-flex flex-row">
-            <a class="navbar-brand p-1" href="index.php">Polls</a>
-            <a class="p-2" href="create.php">Create Poll</a>
-        </div>
-        <div class="d-flex flex-row">
-            <a class="p-2" href="login.php">Login</a>
-            <a class="p-2" href="signup.php">Sign up</a>
-        </div>
+<div class="header navbar">
+    <div class="d-flex flex-row">
+        <a class="navbar-brand p-1" href="index.php">Polls</a>
+        <a class="p-2" href="create.php">Create Poll</a>
     </div>
+    <div class="d-flex flex-row">
+        <?php
 
-    <div class="container">
+        if (isset($_COOKIE["user_token"])) { // user is logged in
+            echo '<a class="p-2" href="myprofile.php">My Profile</a>';
+            echo '<a class="p-2" href="signout.php">Sign out</a>';
+        } else { // user is not logged in
+            echo '<a class="p-2" href="login.php">Login</a>';
+            echo '<a class="p-2" href="signup.php">Sign up</a>';
+        }
+
+        ?>
     </div>
+</div>
+
+<div class="container">
+</div>
 </body>
 </html>
